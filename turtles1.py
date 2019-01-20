5ef99cbf1b8b088ea2a0615491a7c9168e88ab32
Mark Rogers <mrogers@live.ca>
2019-01-19 22:11:45 -0600

    second repo update

--- turtles1.py
+++ turtles1.py
@@ -1,12 +1,31 @@
-import turtle
+import turtle, math
 
-window = turtle.Screen()
-alex = turtle.Turtle()
+class Point:
+    """ Point class represents and manipulates x,y coords. """
 
-while True:
-    alex.forward(200)
-    alex.left(170)
-    if abs(alex.pos()) < 1:
-        break
+    def __init__(self, x=0, y=0):
+        """ Create a new point at the origin """
+        self.x = x
+        self.y = y
 
-window.exitonclick()
+def area(radius):
+    b = math.pi * radius**2
+    return b
+
+def main():
+    window = turtle.Screen()
+    alex = turtle.Turtle()
+
+    testarea = area(3)
+
+    while True:
+        alex.forward(200)
+        alex.left(170)
+        if abs(alex.pos()) < 1:
+            break
+
+    window.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
